b14a2bf2cd48aadc1a4abd97ca28201f755eb52b
Aklaran <Aklaran@users.noreply.github.com>
2025-05-18 12:16:52 -0400

    chore: autoformat

--- nvim/init.lua
+++ nvim/init.lua
@@ -200,10 +200,10 @@ vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower win
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
 -- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
-vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
-vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
-vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
-vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
+vim.keymap.set('n', '<C-S-h>', '<C-w>H', { desc = 'Move window to the left' })
+vim.keymap.set('n', '<C-S-l>', '<C-w>L', { desc = 'Move window to the right' })
+vim.keymap.set('n', '<C-S-j>', '<C-w>J', { desc = 'Move window to the lower' })
+vim.keymap.set('n', '<C-S-k>', '<C-w>K', { desc = 'Move window to the upper' })
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
@@ -1015,8 +1015,8 @@ require('lazy').setup({
 -- vim: ts=2 sts=2 sw=2 et
 
 -- Don't hide links in help pages
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "help",
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'help',
   callback = function()
     vim.opt_local.conceallevel = 0
   end,
