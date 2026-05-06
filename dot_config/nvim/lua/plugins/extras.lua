a8b3845ecd15a831e3c54c4398c851507b330780
Bo Tembunkiart <bo.tembunkiart@tribe.ai>
2026-05-06 13:28:17 -0400

    Enable JavaScript and TypeScript support in Neovim

--- dot_config/nvim/lua/plugins/extras.lua
+++ dot_config/nvim/lua/plugins/extras.lua
@@ -3,6 +3,7 @@ return {
 	{ import = "lazyvim.plugins.extras.lang.json" },
 	{ import = "lazyvim.plugins.extras.lang.markdown" },
 	{ import = "lazyvim.plugins.extras.lang.python" },
+	{ import = "lazyvim.plugins.extras.lang.typescript" },
 	{ import = "lazyvim.plugins.extras.lang.toml" },
 	{ import = "lazyvim.plugins.extras.lang.yaml" },
 	{ import = "lazyvim.plugins.extras.ui.mini-indentscope" },
